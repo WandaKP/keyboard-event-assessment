21478f6f0991e6eb8d44b4e0cf777e8912eae833
Wanda <wandaplaza1031@gmail.com>
2022-12-11 06:45:05 -0500

    Stuck

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+let randomLetter;
+const ol = document.querySelector("ol")
 document.ol.addEventListener('keyup', function(event) {
-
+    const keyPressed = event.key.toLowerCase();
+    let letterFound = false;
+    for(let i = 0; i <= letters; i++) {
+        if (keyPressed === letters[i]) {
+            letterFound = true;
+            const h2Secret = document.createElement(h2);
+            h2Secret.textContent = "SECRECT KEY PRESSED" + [i]
+            ol.appendChild(h2Secret);
+            setup()
+        }
+    }
 })
-
-const randomLetter = letters[Math.floor(map.random() * 26)]
+function setup() {
+    randomLetter = letters[Math.floor(map.random() * letters.length)].toLowerCase
+}
