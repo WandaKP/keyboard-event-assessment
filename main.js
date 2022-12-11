de0fc3275e60755823395cc7a64ebe94ef183029
Wanda <wandaplaza1031@gmail.com>
2022-12-11 01:15:52 -0500

    Stuck

---
+++ main.js
@@ -0,0 +1,7 @@
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+document.ol.addEventListener('keyup', function(event) {
+
+})
+
+const randomLetter = letters[Math.floor(map.random() * 26)]
