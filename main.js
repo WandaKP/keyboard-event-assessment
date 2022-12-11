92139c7d4068dc7a7939128b80fb90726029751a
Wanda <wandaplaza1031@gmail.com>
2022-12-11 15:41:19 -0500

    Fixed

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let randomLetter;
+let randomLetter = letters[Math.floor(Math.random() * letters.length)]
 const ol = document.querySelector("ol")
-document.ol.addEventListener('keyup', function(event) {
-    const keyPressed = event.key.toLowerCase();
-    let letterFound = false;
-    for(let i = 0; i <= letters; i++) {
-        if (keyPressed === letters[i]) {
-            letterFound = true;
-            const h2Secret = document.createElement(h2);
-            h2Secret.textContent = "SECRECT KEY PRESSED" + [i]
-            ol.appendChild(h2Secret);
-            setup()
-        }
+document.body.addEventListener('keyup', function(event) {
+    const keyPressed = event.key;
+    if( keyPressed === randomLetter) {
+        randomLetter = letters[Math.floor(Math.random() * letters.length)];
+        const h2 = document.createElement('h2');
+        h2.innerHTML = "SECRECT KEY PRESSED " + randomLetter
+        ol.appendChild(h2);
     }
 })
-function setup() {
-    randomLetter = letters[Math.floor(map.random() * letters.length)].toLowerCase
-}
